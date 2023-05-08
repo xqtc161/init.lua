eff5f898ee0bffddc5ad115ca0351690516518b2
xqtc161 <till.franke@outlook.com>
2023-05-08 22:49:12 +0200

    keybinds

--- lua/xqtc/remap.lua
+++ lua/xqtc/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
+
+vim.keymap.set("n", "<leader>w", vim.cmd.FormatWrite)
+vim.keymap.set("n", "<leader>hh", vim.cmd.Alpha)
