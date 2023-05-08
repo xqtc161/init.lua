0e188a9fd29b0adb5f51597d1d73a45b5b6a91cd
Tila Franke <xqtc@tutanota.com>
2023-05-08 17:54:15 +0200

    Added start screen and set up formatting for python and lua

--- lua/xqtc/lazy.lua
+++ lua/xqtc/lazy.lua
@@ -1,61 +1,72 @@
 require("lazy").setup({
-	"rebelot/kanagawa.nvim",  
-    {
-        'akinsho/toggleterm.nvim',
-        version = "*",
-        config = true
-    },
+	"rebelot/kanagawa.nvim",
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+	},
 	{
 		"andweeb/presence.nvim",
 		lazy = false,
 	},
-	{"nvim-telescope/telescope.nvim"},
-	{"nvim-lua/plenary.nvim"},
-	{"nvim-treesitter/nvim-treesitter"},
-	{"nvim-treesitter/playground"},
-	{"mbbill/undotree"},
-	{"tpope/vim-fugitive"},
+	{ "nvim-telescope/telescope.nvim" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "nvim-treesitter/playground" },
+	{ "mbbill/undotree" },
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
+		"mhartington/formatter.nvim",
+		lazy = false,
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"goolord/alpha-nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("after.plugin.alpha-tea").config)
+		end,
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
 		dependencies = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			build = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
 			},
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-path'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-path" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
 	},
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies= { 'nvim-tree/nvim-web-devicons', opt = true },
-    },
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        opts = {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.dirman"] = { -- Manages Neorg workspaces
-                config = {
-                    workspaces = {
-                        notes = "~/notes",
-                    },
-                },
-            },
-        },
-    },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-}
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	},
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		opts = {
+			load = {
+				["core.defaults"] = {}, -- Loads default behaviour
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.dirman"] = { -- Manages Neorg workspaces
+					config = {
+						workspaces = {
+							notes = "~/notes",
+						},
+					},
+				},
+			},
+		},
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
 })
