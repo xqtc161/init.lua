1f6ddfeb8d9f85ecfd22d8d44bfe178925d667de
xqtc161 <xqtc@tutanota.com>
2023-05-09 10:25:28 +0200

    fixed git add keybind

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,6 +1,10 @@
 vim.keymap.set("n", "<leader>Gs", ":Git status<CR>");
 vim.keymap.set("n", "<leader>Gf", ":Git fetch<CR>");
-vim.keymap.set("n", "<leader>Ga", ":Git add .<CR> Added files");
+--vim.keymap.set("n", "<leader>Ga", ":Git add .<CR> \"Added files\"");
+vim.keymap.set("n", "<leader>Ga", function ()
+    vim.cmd(":Git add .")
+    print("Files added")
+end) 
 vim.keymap.set("n", "<leader>Gc", ":Git commit<CR>");
 vim.keymap.set("n", "<leader>GP", ":Git push<CR>");
 vim.keymap.set("n", "<leader>Gp", ":Git pull<CR>");
