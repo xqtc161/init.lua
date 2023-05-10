2f1ea7c151db126a9c3453c6f90c0b2d53b40b0f
xqtc161 <xqtc@tutanota.com>
2023-05-11 01:58:49 +0200

    Added makeshift Filetree sidepane thingy :3c

--- lua/xqtc/remap.lua
+++ lua/xqtc/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
+
+
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>es", ":Lexplore | :vertical resize 20<CR>")
 
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
