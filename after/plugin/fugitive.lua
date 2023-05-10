8f4addb3760f44345c6506b1457fde3bd7f37129
xqtc161 <till.franke@outlook.com>
2023-05-10 19:53:47 +0200

    More remapssss yeah yeah

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -4,8 +4,16 @@ vim.keymap.set("n", "<leader>Gf", ":Git fetch<CR>");
 vim.keymap.set("n", "<leader>Ga", function ()
     vim.cmd(":Git add .")
     print("Files added")
-end) 
+end)
+
+
 vim.keymap.set("n", "<leader>Gc", ":Git commit<CR>");
 vim.keymap.set("n", "<leader>GP", ":Git push<CR>");
 vim.keymap.set("n", "<leader>Gp", ":Git pull<CR>");
-vim.keymap.set("n", "<leader>Gd", ":Git diff<CR>");
+vim.keymap.set("n", "<leader>Gd", ":Git diff " .. vim.api.nvim_get_current_buf() .. "<CR>");
+
+vim.keymap.set("n", "<leader>Gd", function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_buf_name = vim.fn.bufname(current_buf)
+  vim.cmd(":Git diff " .. current_buf_name)
+end, {silent = true})
--- lua/xqtc/remap.lua
+++ lua/xqtc/remap.lua
@@ -8,5 +8,18 @@ vim.keymap.set("n", "<C-Left>", "<c-w>h")
 vim.keymap.set("n", "<C-Up>", "<c-w>k")
 vim.keymap.set("n", "<C-Down>", "<c-w>j")
 
+-- move selection linewise
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- when pasting on selected text; selected text gets send to void buffer while original copied text is preserved in the clipoard
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- make current file executable
+vim.keymap.set("x", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 vim.keymap.set("n", "<leader>w", vim.cmd.FormatWrite)
 vim.keymap.set("n", "<leader>hh", vim.cmd.Alpha)
