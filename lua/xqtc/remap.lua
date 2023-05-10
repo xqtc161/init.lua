a19bdc3ebe0d4878bda717f008e6d2ce5dbebd8e
Tila Franke <tila.franke@kernelconcepts.de>
2023-05-10 17:07:42 +0200

    Added arrow key support

--- lua/xqtc/remap.lua
+++ lua/xqtc/remap.lua
@@ -3,7 +3,10 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-
+vim.keymap.set("n", "<C-Right>", "<c-w>l")
+vim.keymap.set("n", "<C-Left>", "<c-w>h")
+vim.keymap.set("n", "<C-Up>", "<c-w>k")
+vim.keymap.set("n", "<C-Down>", "<c-w>j")
 
 vim.keymap.set("n", "<leader>w", vim.cmd.FormatWrite)
 vim.keymap.set("n", "<leader>hh", vim.cmd.Alpha)
