a517a9df8ce7003598ba1bf1902443facab2b39d
xqtc161 <till.franke@outlook.com>
2023-05-18 16:10:49 +0200

    Added fullscreen keybind and some further neovide config

--- lua/xqtc/neovide.lua
+++ lua/xqtc/neovide.lua
@@ -1,3 +1,14 @@
 if vim.g.neovide then
 	vim.g.neovide_cursor_vfx_mode = "railgun"
+
+    vim.g.neovide_profiler = false 
+	vim.g.neovide_hide_mouse_when_typing = true 
+	-- Fullscreen keymapping
+	vim.keymap.set("n", "<leader>FF", function()
+		if vim.g.neovide_fullscreen then
+			vim.g.neovide_fullscreen = false
+		else
+			vim.g.neovide_fullscreen = true
+		end
+	end)
 end
