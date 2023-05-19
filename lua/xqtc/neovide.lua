7e949b3d7a1c08f2ce8c9a56b91b489d5daccaa3
xqtc161 <xqtc@tutanota.com>
2023-05-19 19:57:14 +0200

    Neovide particle config

--- lua/xqtc/neovide.lua
+++ lua/xqtc/neovide.lua
@@ -1,6 +1,8 @@
 if vim.g.neovide then
-	vim.g.neovide_cursor_vfx_mode = "railgun"
-
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    
+    
+    vim.g.neovide_cursor_vfx_particle_density = 30.0
     vim.g.neovide_transparency = 0.8
 
     vim.g.neovide_profiler = false 
