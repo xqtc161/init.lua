db65897fe9d0f68e27f8263c410cd0d4b1a1a806
xqtc161 <xqtc@tutanota.com>
2023-10-08 17:50:54 +0200

    Neotree shows on startup

--- lua/xqtc/init.lua
+++ lua/xqtc/init.lua
@@ -3,14 +3,14 @@ require("xqtc.remap")
 -- lazy.nvim setup/init
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("xqtc.lazy")
@@ -18,3 +18,4 @@ require("lazy").setup(plugins, opts)
 -- lazy.mvim END
 require("xqtc.neovide")
 require("xqtc.set")
+vim.cmd.Neotree()
